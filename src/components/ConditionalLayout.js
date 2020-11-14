670acbc419def31113dbe59d1395708f9eba89be
mustafa-synectiks <35029729+mustafa-synectiks@users.noreply.github.com>
2020-11-14 14:24:25 +0530

    Layout was mis-spelled and now it is rectified

--- src/components/ConditionalLayout.js
+++ src/components/ConditionalLayout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from './Layout';
+import Layout from './layout';
 import { Link, ModalRoutingContext } from 'gatsby-plugin-modal-routing';
 // import './nav.css'
 
